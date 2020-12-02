6964783ef95a7a41377d6d3871825b989179166d
samrudhi93 <58676930+samrudhi93@users.noreply.github.com>
2020-12-02 13:32:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  8 17:06:01 2019
+
+@author: prithvi
+"""
+import flask
+from flask import Flask, request , jsonify, render_template
+import jinja2
+import numpy as np
+import pandas as pd
+import pickle
+
+from flask import Flask, request,jsonify,render_template
+import pickle
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+filename = 'model_gym_data.pkl'
+model = pickle.load(open(filename, 'rb'))
+
+@app.route('/')
+def man():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    data1 = request.form['a']
+    data2 = request.form['b']
+    data3 = request.form['c']
+    arr = np.array([[data1, data2, data3]])
+    pred = model.predict(arr)
+    return render_template('after.html',data=pred)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
